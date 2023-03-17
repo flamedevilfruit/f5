2276ca0e3a6b71820fedda042d431b1885c16d04
Manda Koita <manda@MacBook-Air-de-Manda.local>
2023-03-17 14:38:53 +0100

    in progress

--- front/js/product.js
+++ front/js/product.js
@@ -35,19 +35,74 @@ function getArticle() {
             itemDescription.innerHTML = article.description;
 
             //Boucle qui nous aide à parcourir les items dans l'objet colors enfin de facilité le choix de l'utilisateur
-            /*for (let colors in article.colors) {
-        }*/
-
-        } else {
-            //console.log("Something went wrong");
-            alert("Something went wrong");
-            document.location.href = "index.html";
-        } 
+            for (let i = 0; i < article.colors.length; i++) {
+                let itemColor = document.createElement("option");
+                document.querySelector("#colors").appendChild(itemColor);
+                itemColor.value = article.colors[i];
+                itemColor.innerHTML = article.colors[i];
+            }
+        }
     });
 }
 getArticle();
 
 //variable permettant au choix de l'utilisateur
 
-/*const quantityChoice = document.querySelector("#quantity");
-const colorsChoice = document.querySelector("#colors");*/
+// eslint-disable-next-line no-unused-vars
+function userChoice(article) {
+    const quantityChoice = document.querySelector("#quantity");
+    const colorChoice = document.querySelector("#colors");
+
+    //Si l'utilisateur a choisi un produit
+    const addToCard = document.querySelector("#addToCard");
+
+    addToCard.addEventListener("click", () => {
+        if (quantityChoice.value > 0 && quantityChoice.value <= 100) {
+            var colorPicked = colorChoice.value;
+            var quantityPicked = quantityChoice.value;
+
+            //creer un objet avec les données de l'article et de la quantité
+            var productObj = {
+                idProduct: article.idProduct,
+                productColor: colorPicked,
+                productQuantity: quantityPicked,
+                productName: article.name,
+                productDescription: article.description,
+                productimg: article.imageUrl,
+                productAlt: article.altTxt,
+            };
+            //console.log(productObj);
+            //recuperer le panier du localstorage
+            var itemsInLocalStorage = JSON.parse(localStorage.getItem("produit"));
+
+            //pop up de confirmation
+            const confirmationPopup = () => {
+                if (window.confirm(`Votre commande de ${quantityPicked} ${article.name} de couleur ${colorPicked} au prix de ${article.price} € a bien été ajouté au panier.
+                OK pour consultez le panier, ANNULER pour continuer`)) {
+                    window.location.href = "cart.html";
+                }
+            };
+
+            /*/confirmation d'envoie des prodiots dans le panier 
+            if (itemInLocalStorage) {
+                const resultaFind = itemsInLocalStorage.find(item => item.idproduct === article.idProduct && item.productColor === colorPicked);
+                }
+                //if(resultaFind)
+            }*/
+            const resultaFind = itemsInLocalStorage.find(item => item.idProduct === idProduct && item.productColor === colorPicked);
+            
+            if (resultaFind) {
+                var newQuantity = parseInt(resultaFind.productQuantity) + parseInt(productObj.productQuantity);
+                resultaFind.productQuantity = newQuantity;
+                localStorage.setItem("produit", JSON.stringify(itemsInLocalStorage));
+                confirmationPopup();
+                console.log(resultaFind);
+            } else {
+                itemsInLocalStorage = [];
+                itemsInLocalStorage.push(productObj);
+                localStorage.setItem("produit", JSON.stringify(itemsInLocalStorage));
+                confirmationPopup();
+            }
+        }
+    });
+}
